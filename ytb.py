26ba2a7772aabdd58df5e732b4fa3ddcf0b8bc0e
Ameaca <7kevinla@gmail.com>
2022-07-21 15:51:05 +0100

    V1 END: NEED TESTES helpcmd.py: - Remove, menu.py is the new method data.py: - Remove the Module Dict to put in ytbdl.py because of non sense ytbdl.py: - Add Types(Module Dict) and change the name - add ClearCMD to clear the cmd windows, cleaner cmd - Create command reader (list of ifs ) - Cleaned code ytb.py: - Create functions to download commands.txt: - delete for unnecessary use fileUtility.py: - Update infos help.py: - Change description

--- ytb.py
+++ ytb.py
@@ -1,5 +1,7 @@
 from pytube import YouTube, Playlist
+from data import DEFAULTS
 
+FolderLoc = DEFAULTS['FolderLoc']
 
 def IsPlaylist(link):
     isPlaylist = False
@@ -13,3 +15,43 @@ def IsPlaylist(link):
         except:
             isPlaylist = None
     return isPlaylist
+
+
+def BestAudioLink(link):
+    yt = YouTube(link)
+    print(f"Downloading: {yt.title}")
+    yt.streams.get_audio_only().download(output_path=FolderLoc)
+
+
+def BestAudioPlaylist(link):
+    yt = Playlist(link)
+    for v in yt.videos:
+        print(f"Downloading: {v.title}")
+        v.streams.get_audio_only().download(output_path=FolderLoc)
+
+
+def BestVideoLink(link):
+    yt = YouTube(link)
+    print(f"Downloading: {yt.title}")
+    yt.streams.get_audio_only().download(output_path=FolderLoc)
+
+
+def BestVideoPlaylist(link):
+    yt = Playlist(link)
+    for v in yt.videos:
+        print(f"Downloading: {v.title}")
+        v.streams.get_highest_resolution().download(output_path=FolderLoc)
+
+
+def Download(Type, link, isPlaylist):
+    if (Type == "audio"):
+        if (isPlaylist):
+            BestAudioPlaylist(link)
+        else:
+            BestAudioLink(link)
+    else: 
+        if (isPlaylist):
+            BestVideoPlaylist(link)
+        else:
+            BestVideoLink(link)
+    print()
